c899de62f3a0b9af8aa06e8d129359622b5ac0f2
Mukhtarif-PC <ahmadanmukhtarif@gmail.com>
2018-03-05 20:13:50 +0700

    Udah PHP

---
+++ formPendaftaran.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container">
+        <a class="navbar-brand" href="#">"X" UKM</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Beranda</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Berita</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Divisi</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Tentang Kami</a>
+            </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </div>
+        </div>
+    </nav>
+    <div class="container form-section">
+        
+        PENDAFTARAN UKM "X" Universitas Brawijaya
+        <form action="hasilPendaftaran.php" method="POST" enctype="multipart/form-data">
+            <div class="form-group">
+                <label for="namaPendaftar">Nama</label>
+                <input type="text" class="form-control" id="namaPendaftar" placeholder="John Doe" name="nama">
+            </div>
+            <div class="form-group">
+                <label for="nimPendaftar">NIM</label>
+                <input type="text" class="form-control" id="nimPendaftar" placeholder="1xxxxxxxxxxxxx" name="nim">
+            </div>
+            <div class="form-group">
+                <label for="emailPendaftar">Email</label>
+                <input type="email" class="form-control" id="emailPendaftar" placeholder="john@mail.com" name="email">
+            </div>
+            <div class="form-group row">
+                <div class="col">
+                    <label for="tempatLahir">Tempat Lahir</label>
+                    <input type="text" class="form-control" id="tempatLahir" placeholder="Segitiga Bermuda" name="tempatLahir">
+                </div>
+                <div class="col">
+                    <label for="tanggalLahir">Tanggal Lahir</label>
+                    <input type="date" class="form-control" id="tanggalLahir" name="tglLahir">
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="my-1 mr-2" for="agamaPendaftar">Agama</label>
+                <select class="custom-select my-1 mr-sm-2" id="agamaPendaftar" name="agama">
+                    <option selected>Pilihan</option>
+                    <option value="Islam">Islam</option>
+                    <option value="Hindu">Hindu</option>
+                    <option value="Kristen Katholik">Kristen Katholik</option>
+                    <option value="Kristen Protestan">Kristen Protestan</option>
+                    <option value="Konghucu">Konghucu</option>
+                    <option value="Budha">Budha</option>
+                </select>
+            </div>
+            <div class="form-group row">
+                <div class="col-12">
+                    <label for="alamatPendaftar">Alamat</label>
+                    <input type="text" class="form-control" id="alamatPendaftar" placeholder="Jl. Jalan-jalan No. 1" name="alamat">
+                </div>
+                <div class="col">
+                    <label for="kotaPendaftar">Kota/Kabupaten</label>
+                    <input type="text" class="form-control" id="kotaPendaftar" placeholder="Segitiga Bermuda" name="alamatKota">
+                </div>
+                <div class="col">
+                    <label for="provinsiPendaftar">Provinsi</label>
+                    <input type="text" class="form-control" id="provinsiPendaftar" placeholder="Jawa Utara" name="alamatProvinsi">
+                </div>
+                <div class="col">
+                    <label for="kodeposPendaftar">Kode Pos</label>
+                    <input type="text" class="form-control" id="kodeposPendaftar" placeholder="123456" name="alamatKodePos">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="#">Jenis Kelamin</label>
+                <div class="custom-control custom-radio">
+                    <input type="radio" id="jenisKelaminL" name="radioJenisKelamin" class="custom-control-input" checked value="Laki-Laki">
+                    <label class="custom-control-label" for="jenisKelaminL">Laki-laki</label>
+                </div>
+                <div class="custom-control custom-radio">
+                    <input type="radio" id="jenisKelaminP" name="radioJenisKelamin" class="custom-control-input" value="Perempuan">
+                    <label class="custom-control-label" for="jenisKelaminP">Perempuan</label>
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="#">Hobi</label>
+                <div class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input" id="bermainDiLuar" name="hobi[]">
+                    <label class="custom-control-label" for="bermainDiLuar">Bermain di Luar</label>
+                </div>
+                <div class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input" id="bermainDiDalam" name="hobi[]">
+                    <label class="custom-control-label" for="bermainDiDalam">Bermain di Dalam</label>
+                </div>
+                <div class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input" id="tanamanHolic" name="hobi[]">
+                    <label class="custom-control-label" for="tanamanHolic">Tanaman Holic</label>
+                </div>
+                <div class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input" id="tiduranAja" name="hobi[]">
+                    <label class="custom-control-label" for="tiduranAja">Tiduran Aja</label>
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="reasonable">Alasan Ingin Memasuki UKM "X"</label>
+                <textarea class="form-control" id="reasonable" rows="3" name="alasan"></textarea>
+            </div>
+            <div class="form-group">
+                <label for="#">Foto 3x4 Terbaru</label>
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text">Upload</span>
+                    </div>
+                    <div class="custom-file">
+                        <input type="file" class="custom-file-input" id="inputGroupFile01" name="file">
+                        <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
+                    </div>
+                </div>
+            </div>
+            
+            <input type="submit" class="btn btn-primary" value="Submit"/>
+        </form>
+        
+    </div>
+</body>
+</html>
---
+++ hasilPendaftaran.php
@@ -0,0 +1,100 @@
+<?php
+        $nama = $_POST['nama'];
+        $nim = $_POST['nim'];
+        $email = $_POST['email'];
+        $tempatLahir = $_POST['tempatLahir'];
+        $tglLahir = $_POST['tglLahir'];
+        $agama = $_POST['agama'];
+        $alamat = $_POST['alamat'];
+        $alamatKota = $_POST['alamatKota'];
+        $alamatProvinsi = $_POST['alamatProvinsi'];
+        $alamatKodePos = $_POST['alamatKodePos'];
+        $jenisKelamin = $_POST['radioJenisKelamin'];
+        $hobi = $_POST['hobi'];
+        $alasan = $_POST['alasan'];
+        $namaFile = $_FILES['file']['name'];
+        $tmpFile = $_FILES['file']['tmp_name'];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container">
+        <a class="navbar-brand" href="#">"X" UKM</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Beranda</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Berita</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Divisi</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Tentang Kami</a>
+            </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </div>
+        </div>
+    </nav>
+    <div class="container-fluid">
+        <div class="table-responsive">
+            <table class="table">
+                <tr>
+                    <th class="vertical-center">#</th>
+                    <th class="vertical-center">Nama</th>
+                    <th class="vertical-center">NIM</th>
+                    <th class="vertical-center">E-Mail</th>
+                    <th class="vertical-center">TTL</th>
+                    <th class="vertical-center">Agama</th>
+                    <th class="vertical-center">Alamat</th>
+                    <th class="vertical-center">Jenis Kelamin</th>
+                    <th class="vertical-center">Hobi</th>
+                    <th class="vertical-center">Alasan</th>
+                    <th class="vertical-center">Foto</th>
+                </tr>
+                <tr>
+                    <td class="vertical-center">1</td>
+                    <td class="vertical-center"><?php echo $nama ?></td>
+                    <td class="vertical-center"><?php echo $nim ?></td>
+                    <td class="vertical-center"><?php echo $email ?></td>
+                    <td class="vertical-center"><?php echo $tempatLahir.", ".$tglLahir ?></td>
+                    <td class="vertical-center"><?php echo $agama ?></td>
+                    <td class="vertical-center"><?php echo $alamat.", ".$alamatKota.", ".$alamatProvinsi.", ".$alamatKodePos ?></td>
+                    <td class="vertical-center"><?php echo $jenisKelamin ?></td>
+                    <td class="vertical-center"><?php foreach ($hobi as $val) {
+                        echo $val;
+                    } ?></td>
+                    <td class="vertical-center"><?php echo $alasan ?></td>
+                    <td class="vertical-center"><?php move_uploaded_file($tmpFile, "./".$namaFile); ?><img src="<?php echo $namaFile ?>" alt="" class="pas-foto"></td>
+                </tr>
+            </table>
+        </div>
+
+    </div>
+</body>
+</html>
