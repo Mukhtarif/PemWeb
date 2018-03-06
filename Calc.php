ca4fb399ecd46aa80ba98bdfe98474cd4678ae08
Mukhtarif-PC <ahmadanmukhtarif@gmail.com>
2018-03-06 13:25:55 +0700

    Fix Dislay

--- Calc.php
+++ Calc.php
@@ -31,16 +31,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Kalkulator Luas Tabung</title>
 </head>
 <body>
+    <h1>Cari Luas Tabung</h1>
     <form action="" method="post">
-        <input type="number" name="radius">
-        <input type="number" name="tinggi">
+        <label for="radius">Radius : </label>
+        <input type="text" name="radius" id="radius">
+        <label for="tinggi">Tinggi : </label>
+        <input type="text" name="tinggi" id="tinggi">
         <input type="hidden" name="phi" value="3.14">
-        <input type="submit" name="submit">
-        <input type="text" name="volume" value="<?php echo "Volume : ".$volume;?>">
-        <input type="text" name="luas" value="<?php echo " Luas : ".$luas ?>">
+        <input type="submit" name="submit" value="Hitung">
+        <label for="volume">Volume : </label>
+        <input type="text" name="volume" value="<?php echo $volume;?>" id="volume">
+        <label for="volume">Luas Permukaan : </label>
+        <input type="text" name="luas" value="<?php echo $luas ?>" id="luas">
     </form>
 </body>
 </html>
