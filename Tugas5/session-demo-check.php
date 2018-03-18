caa9e0db6c902935deb3cbabf7d6ddcc309104eb
Mukhtarif-PC <ahmadanmukhtarif@gmail.com>
2018-03-18 17:47:46 +0700

    Tugas B Menggunakan Session

--- Tugas5/session-demo-check.php
+++ Tugas5/session-demo-check.php
@@ -1,22 +1,50 @@
-<?php 
+<?php
     session_start();
-    if (isset($_SESSION)) {
-        $waktuSekarang = $_SESSION['date'];
-        if (substr($waktuSekarang, 0, 2) < 10) {
-            echo "Selamat Pagi, ";
-        }
-        elseif (substr($waktuSekarang, 0, 2) >= 10 and substr($waktuSekarang, 0, 2) < 15) {
-            echo "Selamat Siang, ";
-        }
-        elseif (substr($waktuSekarang, 0, 2) >= 15 and substr($waktuSekarang, 0, 2) < 18) {
-            echo "Selamat Sore, ";
-        }
-        elseif (substr($waktuSekarang, 0, 2) >= 18 and substr($waktuSekarang, 0, 2) < 23) {
-            echo "Selamat Malam, ";
-        }
-        echo "Anda mengakses melalui browser ".$_SESSION['webBrowser'];
-    }
-    else {
-        echo "Belum ada session";
-    }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Result</title>
+
+    <style>
+        .this-body{
+            background: url("http://localhost/hello.jpg");
+            background-repeat: no-repeat;
+            background-size: auto;
+        }
+        
+    </style>
+
+</head>
+<body id="body" class="this-body">
+    <?php
+        if (isset($_SESSION)) {
+            $waktuSekarang = $_SESSION['date'];
+            if (substr($waktuSekarang, 0, 2) < 10) {
+                ?><script>document.getElementById("body").style.background = "url('http://localhost/pagi.jpg')"</script> <?php
+                echo "<h1>Selamat Pagi, </h1>";
+            }
+            elseif (substr($waktuSekarang, 0, 2) >= 10 and substr($waktuSekarang, 0, 2) < 15) {
+                ?><script>document.getElementById("body").style.background = "url('http://localhost/siang.jpg')"</script> <?php
+                echo "<h1>Selamat Siang, </h1>";
+            }
+            elseif (substr($waktuSekarang, 0, 2) >= 15 and substr($waktuSekarang, 0, 2) < 18) {
+                ?><script>document.getElementById("body").style.background = "url('http://localhost/sore.jpg')"</script> <?php
+                echo "<h1>Selamat Sore, </h1>";
+            }
+            elseif (substr($waktuSekarang, 0, 2) >= 18 and substr($waktuSekarang, 0, 2) < 23) {
+                ?><script>document.getElementById("body").style.background = "url('http://localhost/malam.jpg')"</script> <?php
+                echo "<h1>Selamat Malam, </h1>";
+            }
+            echo "<h3>Anda mengakses melalui browser, <h3>".$_SESSION['webBrowser'];
+        }
+        else {
+            echo "Belum ada session";
+            ?><script>document.getElementById("body").style.background = "url('http://localhost/PemWeb/hello.jpg')"</script> <?php
+        }
+    ?>
+</body>
+</html>
--- Tugas5/session-demo.php
+++ Tugas5/session-demo.php
@@ -4,8 +4,28 @@
     date_default_timezone_set('Asia/Jakarta');
     $date = date('H:i:s');
     $webBrowser = $_SERVER['HTTP_USER_AGENT'];
-    
+
     $_SESSION['date'] = $date;
     $_SESSION['webBrowser'] = $webBrowser;
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>BOOM</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="jumbotron jumbotron-fluid header">
+        <div class="container">
+        <h1>Lihat Sesi Anda</h1>
+        <p>DISINI LAH TEMPATNYA</p>
+        <input type="button" value="REFRESH" class="button_active" onclick="location.href='session-demo.php';">
+        <input type="submit" value="Cek Sesi" class="button_active" onclick="location.href='session-demo-check.php';">
+    </div>
+</div>
+</body>
+</html>
